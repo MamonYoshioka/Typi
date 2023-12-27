888d4c7f9e8e80c882862144a59f894f211f1cde
Mamon <yoshiokasnstest@gmail.com>
2023-12-28 00:07:29 +0900

    内容修正

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const RANDOM_SENTENCE_URL_API = "https://api.quotable.io/random";
-const typeDisplay = document.getElementById("Display")
+const typeDisplay = document.getElementById("typeDisplay");
 
 /* 非同期処理 */
-function GetSentence() {
-  return fetch(RANDOM_SENTENCE_URL_API).then((response) => response.json()).then((data) => data.content);
+function GetRandomSentence() {
+  return fetch(RANDOM_SENTENCE_URL_API).then((response) => response.json()).then((data) => data.content); 
 }
 
 /* 非同期処理で取得した文章を表示 */
 async function RenderNextSentence() {
-  const sentence = await GetSentence();
-  console.log(sentence)
-
+  const sentence = await GetRandomSentence();
+  console.log(sentence);
+  
   typeDisplay.innerText = sentence;
 }
 
